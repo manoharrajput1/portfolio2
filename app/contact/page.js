bdd4c385bb82e75bf510501f6dc9ad7d86315680
manoharrajput1 <61151886+manoharrajput1@users.noreply.github.com>
2024-11-16 19:32:39 +0530

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -35,7 +35,7 @@ const Contact = () => {
   }
 
   return (
-    <form className= ' flex flex-col items-center m-2'>
+    <form className= ' flex flex-col color="info" items-center m-2'>
       <input className='m-1 text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500' type='text' name='username' value={user.username} onChange={setUserData} placeholder='Username' required />
       <input className='m-1 text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='number' name='mobile' value={user.mobile} onChange={setUserData} placeholder='Contact Number'required />
       <input className='m-1 text-center rounded-lg placeholder-white bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500'  type='email' name='email' value={user.email} onChange={setUserData} placeholder='Email Address' required/>
